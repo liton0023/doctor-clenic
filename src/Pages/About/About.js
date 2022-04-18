f1745fe6c8525140a342c8fa0d65b93c8331aec7
liton0023 <litonmahmud0023@gmail.com>
2022-04-18 20:16:33 +0600

    refresh site

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../../images/Dental_clinic.jpg'
+import logo from '../../images/profile.jpg'
 
 const About = () => {
     return (
@@ -7,36 +7,15 @@ const About = () => {
             <div className="grid lg:grid-cols-2 mt-10">
 
                 <div className="ml-5">
+                    <h1 className='text-8xl'>Rs Mahmud</h1>
                     <h1 className="font-bold text-5xl text-left pb-5 ">My <span className="text-red-700 underline ">Story</span></h1>
-                    <h5 className=" text-4xl pb-5">There are Good Reason to Visit My Chamber</h5>
-                    <p className="text-2xl pb-5">Dentistry was founded in 2005 by john sinha with the vision of developing high quality and affordable health care infrastructure Chicago,Newyork and California.</p>
-                    <p >We offer the entire range of dental treatments and procedures at transparent and printed prices standards for all patients. We are a chain of dental clinics.We are offering specialized treatment by experienced doctor who have care about before and after the treatment.</p>
+                    <h5 className=" text-4xl pb-5">There are Good Reason to My Goal</h5>
+                    <p className="text-xl text-justify pb-5">Now I am Study In Dep. of Math Govt. k.c College,Jhendeinha.My Goal is different.Now i am contious Programming hero web dev course.This is great course forever.Speachilly Jhonker Mahmub Via amazing man leachen every topic very easilly.Now My Goal continu wev devlopment Course And To heard working for contibuite my wise world web devlopment services.That they can remember for ever .And i am say every Poor studen they con not continu their study for money.I say their you con not weast your time,learn  web devlopment and go to work different wey. .</p>
+                    <p >No Pain, No gain: <br />
+                        Have Patience! Work Hard! YOu will surprise yourself, and eventually, you will be proud of yourself.</p>
                 </div>
                 <img className="lg:pl-20 pl-5" src={logo} alt="" />
             </div>
-            <div className="grid lg:grid-cols-4 mt-10 mb-10 gap-4">
-                <div className="pl-4">
-                    <h4 className="text-2xl pb-2 text-blue-300">Talented Doctors Team</h4>
-                    <p className="pt-2 pb-2">We have built our dentistry over the strong pillars of 22 MDS surgeons. Our team has expert professionals who are immensely experienced.</p>
-                    <h6 className="text-red-400">READ MORE</h6>
-                </div>
-                <div className="pl-4">
-                    <h4 className="text-2xl pb-2 text-blue-300">Talented Doctors Team</h4>
-                    <p className="pt-2 pb-2">We have built our dentistry over the strong pillars of 22 MDS surgeons. Our team has expert professionals who are immensely experienced.</p>
-                    <h6 className="text-red-400">READ MORE</h6>
-                </div>
-                <div className="pl-4">
-                    <h4 className="text-2xl pb-2 text-blue-300">Talented Doctors Team</h4>
-                    <p className="pt-2 pb-2">We have built our dentistry over the strong pillars of 22 MDS surgeons. Our team has expert professionals who are immensely experienced.</p>
-                    <h6 className="text-red-400">READ MORE</h6>
-                </div>
-                <div className="pl-4">
-                    <h4 className="text-2xl pb-2 text-blue-300">Talented Doctors Team</h4>
-                    <p className="pt-2 pb-2">We have built our dentistry over the strong pillars of 22 MDS surgeons. Our team has expert professionals who are immensely experienced.</p>
-                    <h6 className="text-red-400">READ MORE</h6>
-                </div>
-            </div>
-
         </div>
     );
 };
